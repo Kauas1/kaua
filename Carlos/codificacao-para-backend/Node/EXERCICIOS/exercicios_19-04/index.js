2e5453f7d7366a969f544cd954de9b69f9c1c725
Kauas1 <kauafidelis7@gmail.com>
2024-05-06 07:37:53 -0300

    Passei as atividades para o git

--- Carlos/codificacao-para-backend/Node/EXERCICIOS/exercicios_19-04/index.js
+++ Carlos/codificacao-para-backend/Node/EXERCICIOS/exercicios_19-04/index.js
@@ -1,40 +1,146 @@
-const http = require('node:http')
-const url = require('node:url')
-const fs = require('node:fs')
-
+import http from 'node:http'
 const PORT = 3333
 
-const server = http.createServer((request, response) => {
+const participants = [
 
-    const urlInfo = require('node:url').parse(request.url, true)
-    const name = urlInfo.query.name
-    const email = urlInfo.query.email
-    if(!name){
-        fs.readFileSync('index.html', (err, data)=> {
-            response.writeHead(200, {'Concent-Type': 'text/html'})
-            response.write(data)
-        return response.end()
+]
+
+const server = http.createServer((req, res) => {
+    const { url, method } = req
+    console.log(`URL: ${url}`)
+
+    //CRUD: Create, Read, Update e Delete
+
+    // Cadastrando Participante
+
+    if (method === 'GET' && url === '/participants') {
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify(participants))
+    } else if (method === 'GET' && url === ('/participants/count')) {
+        const lengthPart = participants.length
+        if (lengthPart === 0) {
+            res.writeHead(400, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify({ message: "Não foi encontrado participantes registrados" }))
+        } else {
+            res.setHeader('Content-Type', 'application/json')
+            res.end(JSON.stringify({ message: `Existem ${lengthPart} participantes cadastrados!`, value: `${lengthPart}` }))
+        }
+
+        
+    } else if (method === 'GET' && url === ('/participants/count/over18')) {
+        const lengthPart = participants.length
+        if (lengthPart === 0) {
+            res.writeHead(400, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify({ message: "Não foi encontrado participantes registrados" }))
+        } else {
+            const over18 = participants.filter(participant => participant.idade >= 18)
+            if (over18.length !== 0) {
+                res.setHeader('Content-Type', 'application/json')
+                res.end(JSON.stringify({ message: `Existem ${lengthPart} participantes, dentre eles ${over18.length} são maiores de 18!`, value: `${over18.length}` }))
+            } else {
+                res.writeHead(400, { 'Content-Type': 'application/json' })
+                res.end(JSON.stringify({ message: "Não foi encontrado participantes registrados com mais de 18" }))
+            }
+        }
+    } else if (method === 'GET' && url === ('/participants/city/most')) {
+        
+        const countCity = participants.reduce((acc, participant) => {
+            acc[participant.city] = (acc[participant.city] || 0) + 1
+            return acc
+        }, {})
+
+        let qntPart = 0
+        let mostPartCity = ''
+        Object.entries(countCity).forEach((city, count) => {
+            if(count > qntPart){
+                qntPart = count
+                mostPartCity = city
+            }
+        }) //transformei o objeto em array
+        
+        res.setHeader('Content-type', 'application/json')
+        res.end(JSON.stringify({
+            "Quantidade total": qntPart,
+            "Cidade com maior numero de participantes": mostPartCity
+        }))
+
+    } else if (method === 'POST' && url === '/participants') {
+        let body = '';
+        req.on('data', (chunk) => {
+            body += chunk
+        }).on('end', () => {
+            const participant = JSON.parse(body)
+            if (participant.idade < 16) {
+                res.writeHead(403, { 'Content-Type': 'application/json' })
+                return res.end(JSON.stringify({ message: "Não pode ter participante menor de 16" }))
+            } else if (participant.password !== participant.confirmPassword) {
+                res.writeHead(400, { 'Content-Type': 'application/json' })
+                return res.end(JSON.stringify({ message: "Senha não condiz" }))
+            } else {
+                participant.id = participants.length + 1
+                participants.push(participant)
+                res.writeHead(201, { 'Content-Type': 'application/json' })
+                return res.end(JSON.stringify({ message: "Participante cadastrado", dados: participant }))
+            }
         })
-    }else{
-        const nameNewLine = name 
-        // WriteFile = Só escrita
-        // appendFile = Escreve e junta
-        fs.appendFile('arquivo.json', JSON.stringify({nome:nameNewLine },null, "\t") , (err)=>{
-            response.writeHead(302,{
-            Location: '/'
-            })
-            return response.end()
-        });
+    } else if (method === 'PUT' && url.startsWith('/participants/')) {
+        const participantId = url.split('/')[2];
+
+        let body = ''
+        req.on('data', (chunk) => {
+            body += chunk
+        }).on('end', () => {
+            const updateParticipant = JSON.parse(body)
+            const index = participants.findIndex(participant => participant.id == participantId)
+            if (index !== -1) {
+                if (updateParticipant.idade < 16) {
+                    res.writeHead(403, { 'Content-Type': 'application/json' })
+                    return res.end(JSON.stringify({ message: "Não pode ter participante menor de 16" }))
+                } else if (updateParticipant.password !== participant.confirmPassword) {
+                    res.writeHead(400, { 'Content-Type': 'application/json' })
+                    return res.end(JSON.stringify({ message: "Senha não condiz" }))
+                } else {
+                    participants[index] = { ...participants[index], ...updateParticipant }
+                    res.setHeader('Content-Type', 'application/json')
+                    res.end(JSON.stringify(participants[index]))
+                }
+            } else {
+                res.writeHead(400, { 'Content-Type': 'application/json' })
+                return res.end(JSON.stringify({ message: "Usuario não encontrado" }))
+            }
+        })
+
+    } else if (method === 'DELETE' && url.startsWith('/participants/')) {
+        const idPart = url.split('/')[2]
+        const index = participants.findIndex((participantes) => participantes.id == idPart)
+        const partFound = participants.find((participantes) => participantes.id == idPart)
+        if (index !== -1) {
+            res.setHeader("Content-Type", "application/json")
+            participants.push(partFound) //envia uma copia do user para o final
+            participants.splice(index, 1) //deleta o index desejado
+            participants.pop() //deleta a copia criada, assim realocando os outros para não ficar um espaço vazio
+            res.end(JSON.stringify("Sucesfull delete"))
+        } else {
+            //retorna erro caso não exista esse index
+            res.writeHead(404, { "Content-Type": "application/json" })
+            res.end(JSON.stringify({ message: "Can't update this user" }))
+        }
+    } else if (method === 'GET' && url.startsWith('/participants/')) {
+        const participantId = url.split('/')[2]
+        const findParticipant = participants.find(participant => participant.id == participantId)
+
+        if (!findParticipant) {
+            res.writeHead(404, { 'Content-Type': 'application/json' })
+            return res.end(JSON.stringify({ message: "Participante não encontrado, espero ter ajudado" }))
+        } else {
+            res.setHeader('Content-Type', 'application/json')
+            return res.end(JSON.stringify(findParticipant))
+        }
+    } else {
+        res.writeHead(404, { "Content-Type": "application/json" })
+        res.end(JSON.stringify({ codigo: 404, message: "Página não encontra" }))
     }
 
-  
-
-    
-    console.log(name)
-
-})
-
-
-server.listen(PORT, ()=>{
-    console.log(`Servidor no PORT: ${PORT}`)
+}).listen(PORT, () => {
+    console.log('Server Open in port ' + PORT)
 })
--- Carlos/codificacao-para-backend/Node/EXERCICIOS/exercicios_26-04/server.js
+++ Carlos/codificacao-para-backend/Node/EXERCICIOS/exercicios_26-04/server.js
@@ -1,26 +1,27 @@
 import http from "http";
-import { parse } from 'url';
+import { URLSearchParams } from 'url';
 import fs from "fs";
 const model = {
-  "nome": "Carlos Wilton",
-  "cargo": "Instrutor",
+  "nome": "José Maria",
+  "cargo": "Professor",
   "cpf": "123.456.789-00",
-  "email": "carlos@example.com",
-  "telefone": "(11) 98765-4321",
+  "email": "joseMaria@example.com",
+  "telefone": "(12) 93456-7890",
   "data_contratacao": "2022-01-10",
   "salario": 4500,
   "habilidades": ["Front-End", "Back-End", "Docker", "SQL"],
   "idade": 18,
-  "senha": "123",
-  "confirmaSenha": "123"
+  "senha": "123456",
+  "confirmaSenha": "123456"
 }
 
 const PORT = 8080;
 
 const server = http.createServer((req, res) => {
   const { url, method } = req;
-  const queryData = parse(req.url, true).query;
-
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type")
   fs.readFile("funcionarios.json", "utf8", (err, data) => {
     if (err) {
       res.writeHead(500, { "Content-Type": "application/json" });
@@ -49,7 +50,7 @@ const server = http.createServer((req, res) => {
         if (!newItem.hasOwnProperty('idade') || !newItem.hasOwnProperty('nome') || !newItem.hasOwnProperty('cargo') || !newItem.hasOwnProperty('cpf') || !newItem.hasOwnProperty('senha') || !newItem.hasOwnProperty('confirmaSenha') || !newItem.hasOwnProperty('email') || !newItem.hasOwnProperty('telefone') || !newItem.hasOwnProperty('data_contratacao') || !newItem.hasOwnProperty('salario') || !newItem.hasOwnProperty('habilidades')) {
           res.writeHead(401, { "Content-Type": "application/json" });
           res.end(
-            JSON.stringify({ message: `Não foi autorizado a criação do empregado, está faltando informações, siga o modelo: ${model}` })
+            JSON.stringify({ message: `Não foi autorizado a criação do empregado, está faltando informações, siga o modelo:`, modelo: model })
           );
         } else {
           if (newItem.idade < 18) {
@@ -84,54 +85,43 @@ const server = http.createServer((req, res) => {
         }
       });
     } else if (url.startsWith("/empregados/") && method === "PUT") {
+      const id = parseInt(url.split('/')[2])
 
-      const id = parseInt(url.split("/")[2]);
-      let body = "";
-      req.on("data", (chunk) => {
-        body += chunk.toString();
-      });
-      req.on("end", () => {
-        const updatedItem = JSON.parse(body);
-        // Procurar o empregado pelo ID e atualizar seus dados
-        const index = jsonData.findIndex((item) => item.id === id);
-        if (index !== -1) {
-          if (!updatedItem.hasOwnProperty('idade') || !updatedItem.hasOwnProperty('nome') || !updatedItem.hasOwnProperty('cargo') || !updatedItem.hasOwnProperty('cpf') || !updatedItem.hasOwnProperty('senha') || !updatedItem.hasOwnProperty('confirmaSenha') || !updatedItem.hasOwnProperty('email') || !updatedItem.hasOwnProperty('telefone') || !updatedItem.hasOwnProperty('data_contratacao') || !updatedItem.hasOwnProperty('salario') || !updatedItem.hasOwnProperty('habilidades')) {
-            res.writeHead(401, { "Content-Type": "application/json" });
-            res.end(
-              JSON.stringify({ message: `Não foi autorizado a atualização do empregado, está faltando informações, siga o modelo: ${model}` })
-            );
-          } if (updatedItem.idade < 18) {
-            res.writeHead(401, { "Content-Type": "application/json" });
-            res.end(
-              JSON.stringify({ message: "Não foi autorizado! Precisa ser maior de 18 anos!" })
-            );
-          } else if (updatedItem.senha !== updatedItem.confirmaSenha) {
-            res.writeHead(401, { "Content-Type": "application/json" });
-            res.end(
-              JSON.stringify({ message: "Não foi autorizado! As senhas não condizem" })
-            );
+      let body = ''
+      req.on('data', (chunk)=>{
+        body += chunk
+      })
+      req.on('end', ()=> {
+        fs.readFile("funcionarios.json", "utf8", (err,data)=>{
+          if(err){
+            res.writeHead(500, {"Content-Type": "application/json"})
+            res.end(({message: "Erro ao ler o arquivo"}))
           }
-          jsonData[index] = { ...jsonData[index], ...updatedItem };
-          fs.writeFile(
-            "funcionarios.json",
-            JSON.stringify(jsonData, null, 2),
-            (err) => {
-              if (err) {
-                res.writeHead(500, { "Content-Type": "application/json" });
-                res.end(
-                  JSON.stringify({ message: "Erro interno do servidor" })
-                );
-                return;
-              }
-              res.writeHead(200, { "Content-Type": "application/json" });
-              res.end(JSON.stringify(jsonData[index]));
+
+          const jsonData = JSON.parse(data)
+          const indexFuncionario = jsonData.findIndex((funcionario)=> funcionario.id === id)
+
+          if(indexFuncionario === -1){
+            res.writeHead(404, {'Content-Type': 'application/json'})
+            res.end(JSON.stringify({message: 'Funcionário não encontrado!'}))
+          }
+
+          const funcionarioAtualizado = JSON.parse(body)
+          funcionarioAtualizado.id = id
+          
+          jsonData[indexFuncionario] === funcionarioAtualizado
+
+          fs.write('funcionarios.json', JSON.stringify(jsonData, null, 2), (err)=>{
+            if(err){
+              res.writeHead(500, {'Content-Type': 'application/json'})
+              res.end(JSON.stringify({message: 'Erro ao ler o arquivo'}))
             }
-          );
-        } else {
-          res.writeHead(404, { "Content-Type": "application/json" });
-          res.end(JSON.stringify({ message: "Empregado não encontrado" }));
-        }
-      });
+            res.writeHead(200, {"Content-Type": "application/json"})
+            res.end(JSON.stringify(funcionarioAtualizado))
+          })
+        })
+      })
+
     } else if (url.startsWith("/empregados/") && method === "DELETE") {
 
       const id = parseInt(url.split("/")[2]);
@@ -159,7 +149,7 @@ const server = http.createServer((req, res) => {
         res.writeHead(404, { "Content-Type": "application/json" });
         res.end(JSON.stringify({ message: "Livro não encontrado" }));
       }
-    } else if (method === 'GET' && url === ('/empregados/count')) {
+    } else if (method === 'GET' && url === ('/empregados/count/')) {
       const lengthPart = jsonData.length
       if (lengthPart === 0) {
         res.writeHead(400, { 'Content-Type': 'application/json' })
@@ -169,44 +159,83 @@ const server = http.createServer((req, res) => {
         res.end(JSON.stringify({ message: `Existem ${lengthPart} participantes cadastrados!`, value: `${lengthPart}` }))
       }
 
-    } else if (method === 'GET' && url.startsWith('/empregados/porCargo')) {
+    } else if (method === 'GET' && url.startsWith('/empregados/porCargo/')) {
       const empregadoCargo = url.split('/')[3]
       const findEmploy = jsonData.filter(dado => dado.cargo == empregadoCargo)
 
-      if (!findEmploy) {
+      if (findEmploy.length == 0) {
         res.writeHead(404, { 'Content-Type': 'application/json' })
         return res.end(JSON.stringify({ message: "Empregado não encontrado, espero ter ajudado" }))
       } else {
         res.setHeader('Content-Type', 'application/json')
         return res.end(JSON.stringify(findEmploy))
       }
-    } else if (method === 'GET' && url.startsWith('/empregados/porHabilidade')) {
-      const empregadoHabi = url.split('/')[3]
-      const findEmploy = jsonData.filter(dado => dado.habilidades.find(habilidades => habilidades == empregadoHabi))
+    } else if (method === 'GET' && url.startsWith('/empregados/porHabilidade/')) {
 
-      if (!findEmploy) {
-        res.writeHead(404, { 'Content-Type': 'application/json' })
-        return res.end(JSON.stringify({ message: "Empregado não encontrado, espero ter ajudado" }))
-      } else {
-        res.setHeader('Content-Type', 'application/json')
-        return res.end(JSON.stringify(findEmploy))
-      }
-    } else if (method === 'GET' && url.startsWith('/empregados/porFaixaSalarial')) {
-      const minSalary = parseFloat(queryData.min);
-      const maxSalary = parseFloat(queryData.max);
-      const funcionariosNaFaixa = jsonData.filter(funcionario => funcionario.salario >= minSalary && funcionario.salario <= maxSalary)
-      if (!funcionariosNaFaixa) {
-        res.writeHead(404, { 'Content-Type': 'application/json' })
-        return res.end(JSON.stringify({ message: "Não foram encontrados funcionarios com essa Faixa salarial!" }))
-      } else {
-        res.setHeader('Content-Type', 'application/json')
-        return res.end(JSON.stringify(funcionariosNaFaixa))
-      }
+  const habilidade = url.split('/')[3]
+      fs.readFile("funcionarios.json", "utf8", (err, data)=>{
+
+        if(err){
+          res.writeHead(500, {"Content-Type": "application/json"})
+          res.end(JSON.stringify({message: "Erro ao ler o arquivo"}))
+        }
+
+        const jsonData = JSON.parse(data)
+
+        const funcionariosPorHabilidade = jsonData.filter((funcionario)=> funcionario.habilidades.includes(habilidade)
+        )
+        
+        if(funcionariosPorHabilidade.length === 0){
+          res.writeHead(404, {"Content-Type": "application/json"})
+          res.end(
+            JSON.stringify({message: "Não existe funcionario com essa habilidade"})
+          )
+          return;
+        }
+        res.writeHead(200, {"Content-Type": "application/json"})
+        res.end(JSON.stringify(funcionariosPorHabilidade))
+      })
+
+      
+
+    } else if (method === 'GET' && url.startsWith('/empregados/porFaixaSalarial/')) {
+      /* Requisições
+         body -> JSON -> POST
+         ROUTE PARAM -> porHabilidade/ValorEnviado ->  PUT, DELETE, PATH, GET
+         Query PARAM -> porFaixaSalarial?valor1=10&valor2=20
+      */
+      const urlParams = new URLSearchParams(url.split('?')[1])
+      const minSalario =  urlParams.get("minSalario")
+      const maxSalario =  urlParams.get("maxSalario")
+
+      fs.readFile("funcionarios.json", "utf8", (err, data)=> {
+        if(err){
+          res.writeHead(500, {"Content-Type": "application/json"})
+          res.end(JSON.stringify({message: "Erro ao ler o arquivo"}))
+        }
+
+        const jsonData = JSON.parse(data)
+        const funcionarioPorFaixaSalarial = jsonData.filter((funcionario)=> funcionario.salario >= minSalario && funcionario.salario <= maxSalario
+        )
+        if(funcionarioPorFaixaSalarial.length === 0){
+          res.writeHead(404, {"Content-Type": "application/json"})
+          res.end(
+            JSON.stringify({message: "Não existe funcionario com essa faixa salarial"})
+          )
+          return
+        }
+        res.writeHead(200, {"Content-Type": "application/json"})
+        res.end(JSON.stringify(funcionarioPorFaixaSalarial))
+
+      })
+
+
     } else if (method === 'GET' && url.startsWith('/empregados/')) {
+
       const empregadoId = url.split('/')[2]
       const findEmploy = jsonData.find(dado => dado.id == empregadoId)
 
-      if (!findEmploy) {
+      if (findEmploy.length == 0) {
         res.writeHead(404, { 'Content-Type': 'application/json' })
         return res.end(JSON.stringify({ message: "Empregado não encontrado, espero ter ajudado" }))
       } else {
@@ -217,6 +246,7 @@ const server = http.createServer((req, res) => {
       res.writeHead(404, { "Content-Type": "application/json" });
       res.end(JSON.stringify({ message: "Rota não encontrada" }));
     }
+
   });
 });
 
